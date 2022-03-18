1fde5505a91a94daddf043ffb3528a0b5a749432
Mladen Perišić <mladen.perisic@comminus.hr>
2022-03-18 09:31:23 +0100

    cleanup commented code

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,12 @@
-//// use regex::Regex;
-use std::path::Path;
-//use rand::Rng;
-// use std::cmp::Ordering;
-use std::fs;
-//use std::io;
-//use dialoguer::Confirm;
 use dialoguer::MultiSelect;
+use std::fs;
+use std::path::Path;
 
 mod file_opretaions;
 
 fn main() {
-    // // let re = Regex::new(r"^[a-zA-Z0-9_]+$").unwrap();
-    // // println!("Hello, world! {}", re.is_match("hello#_world"));
-
-    // println!("Guess the number!");
-
-    // let secret_number = rand::thread_rng().gen_range(1..101);
-    // println!("The secrent number is: {}", secret_number);
-    // loop {
-    //     println!("Please input your guess.");
-
-    //     let mut guess = String::new();
-
-    //     io::stdin()
-    //         .read_line(&mut guess)
-    //         .expect("Failed to read line");
-    //     let guess: u32 = match guess.trim().parse() {
-    //         Ok(num) => num,
-    //         Err(_) => continue,
-    //     };
-    //     println!("You guessed: {}", guess);
-
-    //     match guess.cmp(&secret_number) {
-    //         Ordering::Less => println!("Too small!"),
-    //         Ordering::Greater => println!("Too big!"),
-    //         Ordering::Equal => {
-    //             println!("You win!");
-    //             break;
-    //         }
-    //     }
-    // }
-
-    // if Confirm::new().with_prompt("Do you want to continue?").interact().unwrap() {
-    //     println!("Looks like you want to continue");
-    // } else {
-    //     println!("nevermind then :(");
-    // }
-
-    // let items = vec!["Option 1", "Option 2"];
-    // let chosen : Vec<usize> = MultiSelect::new()
-    //     .items(&items)
-    //     .interact().unwrap();
-
-    //     for item in chosen {
-    //         println!("You chose: {}", items[item]);
-    //     }
-
     //original file
     let mut original_file_path = String::new();
-    // println!("Please input the file path:");
-    // io::stdin()
-    //     .read_line(&mut original_file_path)
-    //     .expect("Failed to read line");
 
     original_file_path = file_opretaions::file_opretaions::read_file_path(
         &original_file_path,
@@ -80,7 +25,6 @@ fn main() {
     let mut files: Vec<String> = Vec::new();
 
     for file in folder_content {
-        //files.push(file.unwrap().path().display().to_string());
         files.push(
             file.unwrap()
                 .file_name()
@@ -88,28 +32,24 @@ fn main() {
                 .into_owned()
                 .to_string(),
         );
-        //println!("File path is: {}", file.unwrap().file_name().to_string_lossy());
     }
 
     let chosen: Vec<usize> = MultiSelect::new().items(&files).interact().unwrap();
 
     //copy to
     let mut copy_file_path = String::new();
-    //println!("Please input where to copy path:");
     copy_file_path = file_opretaions::file_opretaions::read_file_path(
         &copy_file_path,
         "Please input where to copy path:",
     );
 
     for item in chosen {
-        //println!("You chose: {}", files[item]);
         //build original file path including file name
         let full_original_path = path.join(files[item].trim());
 
         //build copy file path including file name
         let new_file_path: String =
             [copy_file_path.to_string(), files[item].to_string()].join("\\");
-        //println!("new file path: {}", new_file_path);
 
         let copy_result = fs::copy(full_original_path, new_file_path.trim());
         match copy_result {
@@ -118,28 +58,5 @@ fn main() {
         }
     }
 
-    file_opretaions::file_opretaions::wait_user_input_message(
-        "We done! We the best!",
-    );
-    //println!("Path is: {}", path.display());
-    // println!("Parent path is: {}", path.parent().unwrap().display());
-
-    //read parent dir
-    // let parent_paths = fs::read_dir(path.parent().unwrap()).unwrap();
-    // for path_file in parent_paths {
-    //     println!("{}", path_file.unwrap().path().display());
-    // }
-
-    // io::stdin()
-    //     .read_line(&mut copy_file_path)
-    //     .expect("Failed to read line");
-
-    // //let copy_result = fs::copy("hello.txt", "hello_copy.txt");
-    // let copy_result = fs::copy(original_file_path.trim(), copy_file_path.trim());
-    // match copy_result {
-    //     Ok(_) => println!("File copied successfully"),
-    //     Err(error) => println!("Error: {}", error),
-    // }
-
-    //println!("Copy result: {}", copy_result.is_ok());
+    file_opretaions::file_opretaions::wait_user_input_message("We done! We the best!");
 }
